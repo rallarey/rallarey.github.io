e8cd85ed754abf4ce8da44537e30bfd9869b6434
Ryan Allarey <60989959+rallarey@users.noreply.github.com>
2021-10-26 16:27:58 -0400

    font, text color and font weights

--- js/home.js
+++ js/home.js
@@ -45,4 +45,26 @@ function clickHandle(evt, jobName) {
     // Display the clicked tab and set it to active.
     document.getElementById(jobName).style.display = "block";
     evt.currentTarget.className += " active";
-  }    
+  }  
+  
+  
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
