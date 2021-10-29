0d359487128029e4873c6041da383541116f7803
Ryan Allarey <60989959+rallarey@users.noreply.github.com>
2021-10-28 20:40:14 -0400

    back to top button

--- js/home.js
+++ js/home.js
@@ -8,48 +8,6 @@ function myFunction() {
     }
 }
 
-//defining sticky function
-window.onscroll = function (){
-    stickyFunction()
-};
-    
-var navbar = document.getElementById("myTopnav");
-var sticky = navbar.offsetTop;
-
-//sticky function   
-function stickyFunction(){
-    if (window.pageYOffset >= sticky){
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky")
-    }
-}
-
-
-document.getElementsByClassName('tablinks')[0].click()
-
-function clickHandle(evt, jobName) {
-    let i, tabcontent, tablinks;
-  
-    // This is to clear the previous clicked content.
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Set the tab to be "active".
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Display the clicked tab and set it to active.
-    document.getElementById(jobName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }  
-  
-  
-
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
@@ -71,4 +29,3 @@ function dropFunction() {
       }
     }
   }
-  
---
+++ js/tabs.js
@@ -0,0 +1,61 @@
+document.getElementsByClassName('tablinks')[0].click()
+
+function clickHandle(evt, jobName) {
+  let i, tabcontent, tablinks;
+  
+  // This is to clear the previous clicked content.
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  
+  // Set the tab to be "active".
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  
+  // Display the clicked tab and set it to active.
+  document.getElementById(jobName).style.display = "block";
+  evt.currentTarget.className += " active";
+}  
+  
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollingFunction()
+};
+  
+function scrollingFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+  
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+/*
+//defining sticky function
+window.onscroll = function (){
+  stickyFunction()
+};
+
+var navbar = document.getElementById("myTopnav");
+var sticky = navbar.offsetTop;
+
+//sticky function   
+function stickyFunction(){
+  if (window.pageYOffset >= sticky){
+      navbar.classList.add("sticky")
+  } else {
+      navbar.classList.remove("sticky")
+  }
+}
+*/
